51dff0385df97f2c2a3dfac5fcfe5ce1ea27eeb9
Justin Hoyt <justinhoyt24@gmail.com>
2023-12-28 11:22:58 -0800

    Update template.ts

--- template.ts
+++ template.ts
@@ -3,24 +3,27 @@
 import * as mod from "https://deno.land/std@0.209.0/assert/mod.ts";
 import { parse } from "https://deno.land/std@0.200.0/flags/mod.ts";
 import $ from "https://deno.land/x/dax@0.36.0/mod.ts";
+import "https://deno.land/x/dax_extras@2.3.0-0.36.0/mod.ts";
 
-async function main(TODO: string): Promise<string> {
-  console.log(await $`ls`.quiet().lines());
-  return TODO;
+interface Args {
+}
+
+async function main(args: Args): Promise<string> {
+  return await $`ls`.quiet().lines();
 }
 
 if (import.meta.main) {
-  const parsedArgs = parse(Deno.args, {
+  const parsedArgs: Args = parse(Deno.args, {
     string: ["color"],
     default: { color: 'blue' },
   });
 
-  console.log(await main(parsedArgs.color));
+  console.log(await main(parsedArgs));
 }
 
-Deno.test('TODO', async () => {
+Deno.test('TODO', () => {
   mod.assertEquals(
-    await main('red'),
-    "red",
+    true,
+    true,
   );
 });
